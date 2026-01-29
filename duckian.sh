f2c877599b57957f85bdcc0652b48158fd9ae44c
DuckAfire <155199080+duckafire@users.noreply.github.com>
2026-01-29 09:32:05 -0300

    [duckian.sh] Add code snippet to OPTIONALLY (not by default) (ba)sh script out from APT Repositories

--- duckian.sh
+++ duckian.sh
@@ -4,6 +4,15 @@
 ####################################################################################################
 
 
+NO_INSTALL_FROM_EXTERN_SCRIPT=1
+# Comment this line to download, and
+# auto-install (BA)SH scripts out from
+# APT Repositories.
+
+
+####################################################################################################
+
+
 # duckian.sh
 # MIT License
 #
@@ -392,6 +401,21 @@ apt autoremove -y
 apt install $APT_CIF $install_packages
 
 unset purge_packages purge_conf install_packages
+
+
+####################################################################################################
+
+
+(
+	set +u -eo pipefail
+
+	if [ -n "$NO_INSTALL_FROM_EXTERN_SCRIPT" ]
+	then
+		# Install bun.js
+		export BUN_INSTALL="$MYBIN_DIR"
+		wget -qO- --max-redirect=99 "https://bun.com/install" | bash
+	fi
+)
 
 
 ####################################################################################################
